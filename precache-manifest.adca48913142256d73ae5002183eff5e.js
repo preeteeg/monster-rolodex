122d3312abcd1218ebc0cdb4bf27b2ce1e8c78e9
Preetee <Preetee@Dhirajs-MacBook-Pro.local>
2019-09-23 14:25:17 +0400

    Updates

---
+++ precache-manifest.adca48913142256d73ae5002183eff5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f09efe510caa6ff8e9a96440d0e75b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f5f5b7dc0d34472fdc16",
+    "url": "/monsters-rolodex/static/css/main.2397a991.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "f5f5b7dc0d34472fdc16",
+    "url": "/monsters-rolodex/static/js/main.8af51930.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
--- src/components/card/card.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './card.styles.css'
-export const Card = props=>(
-<div className="card-container">
-	<img alt="monster"  src={`https://robohash.org/${props.monster.id}?set=set2/size=130x130`} />
-	<h3 className="card-name">{props.monster.name}</h3>
-	<p className="card-detail">{props.monster.email}</p>
-</div>
-)
