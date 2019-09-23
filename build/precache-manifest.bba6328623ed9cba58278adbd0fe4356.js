1f604ce05afb6fb0b85447cba232f058b66d90d9
Preetee <Preetee@Dhirajs-MacBook-Pro.local>
2019-09-23 12:50:01 +0400

    adding files for monster-rolodex app

---
+++ build/precache-manifest.bba6328623ed9cba58278adbd0fe4356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8fe1472984cada85bfdde5524908fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ff2cc081624ca893497",
+    "url": "/monsters-rolodex/static/css/main.2397a991.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monsters-rolodex/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "2ff2cc081624ca893497",
+    "url": "/monsters-rolodex/static/js/main.7954ce6f.chunk.js"
+  },
+  {
+    "revision": "137f4aaae6459cb874ed",
+    "url": "/monsters-rolodex/static/js/runtime-main.2d5d4edd.js"
+  }
+]);
